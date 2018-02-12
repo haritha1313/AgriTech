688a43599d56033c520642ed350da658a6390f70
Haritha <haritha1313@gmail.com>
2018-02-12 17:48:25 +0530

    calculate profit from yield and coc

--- data_clean.py
+++ data_clean.py
@@ -9,15 +9,19 @@ import pandas as pd
 import numpy as np
 import csv
 
+#load csv files
 market_price = pd.read_csv('market.csv')
 standard_price = pd.read_csv('standard.csv')
+coc = pd.read_csv('cost-of-cultivation.csv')
 
+#list of considered crops
 products = ["groundnut", "paddy", "rice", "wheat", "barley", 
             "jowar", "bajra", "maize", "ragi", "gram", "tur", 
             "mustard", "soyabean", "sunflower", 
             "cotton", "jute", "sugarcane"]
 market_edited = market_price[market_price['commodity'].str.lower().isin([x.lower() for x in products])]
 
+#take mean of all varieties of one crop
 grouped_market = market_edited.groupby(['state', 'commodity']).agg(['mean']).reset_index()
 grouped_market.columns = ["".join(x) for x in grouped_market.columns.ravel()]
 
@@ -25,9 +29,14 @@ grouped_market.columns = ["".join(x) for x in grouped_market.columns.ravel()]
 grouped_market['modal_pricemean'] = grouped_market['modal_pricemean'].apply(lambda x: float(x)/100)
 standard_price['price'] = standard_price['price'].apply(lambda x: float(x)/100)
 
+#convert to dictionary of crop to profit or cost
 std_prices = dict(zip(standard_price.standard, standard_price.price))
+coc_dict = dict(zip(coc.crop, coc.cost))
 
+#state vs crop matrix
 yield_mat = pd.read_csv('state-crop-yield.csv')
+
+#state and crop lists
 states = yield_mat['state']
 crops = list(yield_mat.columns)[1:]
 
@@ -35,21 +44,31 @@ yield_np_mat = yield_mat.as_matrix()
 
 prediction_table = list()
 
-for i in range(1,yield_np_mat.shape[0]-1):
+for i in range(0,yield_np_mat.shape[0]-1):
     for j in range(1,yield_np_mat.shape[1]):
         if(yield_np_mat[i][j]!=0.0):
+	    #if yielding in current state then calculate profit
             rs_per_kg = grouped_market[(grouped_market['state']==states[i]) & (grouped_market['commodity'] == crops[j-1])]['modal_pricemean']
             if(rs_per_kg.shape[0]==1):
-                val = (yield_np_mat[i][j]) * rs_per_kg.values[0]
+		#use market price data
+                diff = ((yield_np_mat[i][j]) * rs_per_kg.values[0]) - coc_dict[str(crops[j-1].lower())]
+                val = diff if diff>0.0 else 0.0
             else:
-                val = (yield_np_mat[i][j]) * std_prices[str(crops[j-1].lower())]
+		#use approximated standard price
+                diff = ((yield_np_mat[i][j]) * std_prices[str(crops[j-1].lower())]) - coc_dict[str(crops[j-1].lower())]
+                val = diff if diff>0.0 else 0.0
             
+	    #append all results to a table
             prediction_table.append([states[i], crops[j-1], val])
         else:
+	    #states which cant produce the given crop
             print("**", states[i],crops[j-1])
+            prediction_table.append([states[i], crops[j-1], -1])
             
-print(prediction_table)
+#print(prediction_table)
 
-with open("state-yield-data.csv", "wb") as f:
+#save to csv
+with open("state-profit-data.csv", "wb") as f:
     writer = csv.writer(f)
     writer.writerows(prediction_table)
+    
